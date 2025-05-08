875faca22d588a042c43d57e7e3588633badfa26
Jason Browne <jason@browne.me.uk>
2025-05-08 14:26:09 +0100

    Calculate page number in journal action

--- src/actions/journal.php
+++ src/actions/journal.php
@@ -4,10 +4,12 @@ namespace jbrowneuk;
 
 class Journal implements Page
 {
-    public function render($pdo, $renderer, $pageParams)
+    public function render(\PDO $pdo, PortfolioRenderer $renderer, array $pageParams)
     {
+        $page = $this->calculatePage($pageParams);
+
         try {
-            $posts = get_posts($pdo);
+            $posts = get_posts($pdo, $page);
         } catch (\PDOException $ex) {
             die($ex->getMessage());
         }
@@ -15,5 +17,17 @@ class Journal implements Page
         $renderer->setPageId('journal');
         $renderer->assign('posts', $posts);
         $renderer->displayPage('post-list');
+    }
+
+    // [TODO] convert to helper function so all pages can use it
+    private function calculatePage(array $pageParams) {
+        $index = array_find_key($pageParams, fn($item) => $item === 'page');
+
+        // Calculate whether the next param after 'page' is numeric
+        if (array_key_exists($index + 1, $pageParams) && is_numeric($pageParams[$index + 1])) {
+            return (int)$pageParams[$index + 1];
+        }
+
+        return 1;
     }
 }
