39389be5cc05b81d0834dbcc3788a441c53d9dff
Jason Browne <jason@browne.me.uk>
2025-05-08 14:24:56 +0100

    Implement functionality to get total post count

--- src/database/posts.php
+++ src/database/posts.php
@@ -3,6 +3,27 @@
 namespace jbrowneuk;
 
 function get_posts($pdo)
+/**
+ * Gets the total post count, optionally scoped to a specific tag
+ *
+ * @param \PDO $pdo a connected PDO object
+ * @param ?string $tag (optional) tag to scope the count to
+ *
+ * @return int total count of posts in the search scope
+ */
+function get_post_count(\PDO $pdo, ?string $tag = null)
+{
+    if ($tag != null && strlen($tag) > 0) {
+        $statement = $pdo->prepare('SELECT count(post_id) AS total FROM posts WHERE tags LIKE :tag');
+        $statement->execute(['tag' => "%$tag%"]);
+    } else {
+        $statement = $pdo->query('SELECT count(post_id) AS total FROM posts');
+    }
+
+    $row = $statement->fetch(\PDO::FETCH_ASSOC);
+    return $row['total'];
+}
+
 {
     $itemsPerPage = 5;
     $statement = $pdo->query('SELECT * FROM posts ORDER BY timestamp DESC LIMIT ' . $itemsPerPage);
